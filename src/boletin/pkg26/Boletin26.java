33c139a1af59947ea1af4fea6066951373b8e7da
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-04-23 08:46:14 +0200

    Git commit.

---
+++ src/boletin/pkg26/Boletin26.java
@@ -0,0 +1,18 @@
+
+package boletin.pkg26;
+
+/**
+ *
+ * @author David
+ */
+public class Boletin26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Boletin_26 obx=new Boletin_26();
+        obx.setVisible(true);
+    }
+    
+}
